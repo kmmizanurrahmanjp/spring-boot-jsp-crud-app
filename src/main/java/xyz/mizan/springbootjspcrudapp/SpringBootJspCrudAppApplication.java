0d07255e8b3efc1efc9488b77781666f92a31cbe
kmmizanurrahmanjp <https.mizan@gmail.com>
2018-08-15 00:52:41 +0600

    initial commit

---
+++ src/main/java/xyz/mizan/springbootjspcrudapp/SpringBootJspCrudAppApplication.java
@@ -0,0 +1,12 @@
+package xyz.mizan.springbootjspcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJspCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJspCrudAppApplication.class, args);
+	}
+}
